c96e64babb49a4526172667045fb88cba442e71e
takeru <post8zntyn@gmail.com>
2022-07-31 16:30:44 +0000

    課題2電卓修正080122

--- main-calculator.js
+++ main-calculator.js
@@ -7,36 +7,36 @@
 function calc(v){
     /*document（全体）内にあるid=`result`要素を取得し、
     そのvalue値に引数vをどんどん入れていく*/
-    document.getElementById(`result`).value += v;
+    document.getElementById('result').value += v;
 }
 
 //先頭に0を表示させなくする("0"用)少数問題がまだある...
 function zero(z){
   
-  const mid_way = document.getElementById(`result`).value;
-  const last_child = mid_way.slice(-1);
+  const mid_way = document.getElementById('result').value;
+  let last_child = mid_way.slice(-1);
   /*0入力について、先頭に0はok(少数など0.0~)、000~もok(1000や0.000~など)、
   しかし小数点なしの00はダメ（000.00など）*/
   
   switch (last_child){
     //末尾が空欄・各演算子の場合、空欄で返す...しかしこのままでは少数入力ができない...
     case "":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += "";
       break;
     case "+":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += "";
       break;
     case "-":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += "";
       break;
     case "*":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += "";
       break;
     case "/":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += "";
       break;
     default:
-      document.getElementById(`result`).value += z;
+      document.getElementById('result').value += z;
     
   }
   
@@ -46,143 +46,149 @@ function zero(z){
 
 //小数点用（.）
 function dod(d){
-  const mid_way = document.getElementById(`result`).value;
-  const last_child = mid_way.slice(-1);
+  const mid_way = document.getElementById('result').value;
+  let last_child = mid_way.slice(-1);
   
   switch(last_child){
-    //末尾が空欄、各演算子、.の場合は空欄を入力する
+    /*末尾が空欄、各演算子、.の場合は手前に0を入力する
+    少数を入力するときは、"."入力で表示するようにする*/
     case "":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += '0' + d;
       break;
     case "+":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += '0' + d;
       break;
     case "-":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += '0' + d;
       break;
     case "*":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += '0' + d;
       break;
     case "/":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += '0' + d;
       break;
+    //末尾が"."の場合、空欄を入力する
     case ".":
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += "";
       break;
       /*先頭空欄で00.1~のような入力をさせたくないが、そうすると100.~のようなことが
       入力できなくなってしまう...*/
     default:
-      document.getElementById(`result`).value += d;
+      document.getElementById('result').value += d;
       break;
-    
   }
-  
-  
-  
-  
 }
 
 
 
 //演算子用(+＆-)
 function addsub(a){
-  const mid_way = document.getElementById(`result`).value;
-  const last_child = mid_way.slice(-1);
+  const mid_way = document.getElementById('result').value;
+  let last_child = mid_way.slice(-1);
   
   switch (last_child){
     case "+":
       //末尾が+の場合、削除して再入力する
-      const last_plus = document.getElementById(`result`).value.slice(0,-1);
-      document.getElementById(`result`).value = last_plus;
-      document.getElementById(`result`).value += a;
+      const last_plus = document.getElementById('result').value.slice(0,-1);
+      document.getElementById('result').value = last_plus;
+      document.getElementById('result').value += a;
       break;
     case "-":
       //末尾が-の場合、削除して再入力する
-      const last_minus = document.getElementById(`result`).value.slice(0,-1);
-      document.getElementById(`result`).value = last_minus;
-      document.getElementById(`result`).value += a;
+      const last_minus = document.getElementById('result').value.slice(0,-1);
+      document.getElementById('result').value = last_minus;
+      document.getElementById('result').value += a;
       break;
     case "*":
       //末尾が*の場合、削除して再入力する
-      const last_multi = document.getElementById(`result`).value.slice(0,-1);
-      document.getElementById(`result`).value = last_multi;
-      document.getElementById(`result`).value += a;
+      const last_multi = document.getElementById('result').value.slice(0,-1);
+      document.getElementById('result').value = last_multi;
+      document.getElementById('result').value += a;
       break;
     case "/":
       //末尾が/の場合、削除して再入力する
-      const last_divi = document.getElementById(`result`).value.slice(0,-1);
-      document.getElementById(`result`).value = last_divi;
-      document.getElementById(`result`).value += a;
+      const last_divi = document.getElementById('result').value.slice(0,-1);
+      document.getElementById('result').value = last_divi;
+      document.getElementById('result').value += a;
+      break;
+    case ".":
+      //末尾が.の場合、入力不可する
+      document.getElementById('result').value += "";
       break;
       //末尾が演算子以外の場合、そのまま入力する
     default:
-      document.getElementById(`result`).value += a;
+      document.getElementById('result').value += a;
       break;
   }
 }
 
 //演算子用(*＆/)
 function multidiv(m){
-  const mid_way = document.getElementById(`result`).value;
-  const last_child = mid_way.slice(-1);
+  const mid_way = document.getElementById('result').value;
+  let last_child = mid_way.slice(-1);
   
   switch (last_child){
     case "":
       //末尾が空欄の時は、入力できない
-      document.getElementById(`result`).value += "";
+      document.getElementById('result').value += "";
       break;
     case "+":
       //末尾が+の場合
-      const last_plus = document.getElementById(`result`).value.slice(0,-1);
+      let last_plus = document.getElementById('result').value.slice(0,-1);
       //末尾の+を削除後、もし空欄の場合は入力しない
       if(last_plus == ""){
-        document.getElementById(`result`).value += "";
+        document.getElementById('result').value += "";
         //空欄じゃない場合、そのまま入力
       } else {
-        document.getElementById(`result`).value = last_plus;
-        document.getElementById(`result`).value += m;
+        document.getElementById('result').value = last_plus;
+        document.getElementById('result').value += m;
       }
       break;
     case "-":
       //末尾が-の場合、削除して再入力する
-      const last_minus = document.getElementById(`result`).value.slice(0,-1);
+      let last_minus = document.getElementById('result').value.slice(0,-1);
       //末尾の-を削除後、もし空欄の場合は入力しない
       if(last_minus == ""){
-        document.getElementById(`result`).value += "";
+        document.getElementById('result').value += "";
         //空欄じゃない場合、そのまま入力
       } else {
-        document.getElementById(`result`).value = last_minus;
-        document.getElementById(`result`).value += m;
+        document.getElementById('result').value = last_minus;
+        document.getElementById('result').value += m;
       }
       break;
     case "*":
       //末尾が*の場合、削除して再入力する
-      const last_multi = document.getElementById(`result`).value.slice(0,-1);
-      document.getElementById(`result`).value = last_multi;
-      document.getElementById(`result`).value += m;
+      let last_multi = document.getElementById('result').value.slice(0,-1);
+      document.getElementById('result').value = last_multi;
+      document.getElementById('result').value += m;
       break;
     case "/":
       //末尾が/の場合、削除して再入力する
-      const last_divi = document.getElementById(`result`).value.slice(0,-1);
-      document.getElementById(`result`).value = last_divi;
-      document.getElementById(`result`).value += m;
+      let last_divi = document.getElementById('result').value.slice(0,-1);
+      document.getElementById('result').value = last_divi;
+      document.getElementById('result').value += m;
+      break;
+    case ".":
+      //末尾が.の場合、入力不可する
+      document.getElementById('result').value += "";
       break;
       //末尾が演算子以外の場合、そのまま入力する
     default:
-      document.getElementById(`result`).value += m;
+      document.getElementById('result').value += m;
       break;
   }
 }
 
+
 //AutoClear用
 function reset(){
-    document.getElementById(`result`).value = "";
+    document.getElementById('result').value = "";
 }
+
 
 //=用
 function equal(){
-  const result = document.getElementById(`result`).value;
+  const result = document.getElementById('result').value;
   //console.log(eval(result));←検証画面で表示確認
-  document.getElementById(`result`).value = eval(result);
-  
+  document.getElementById('result').value = eval(result);
 }
